eadf488d224a1c7f82a87beff2f512db56214a2a
Ovieh <ovieh.mosley@gmail.com>
2018-01-12 10:07:09 -0500

    replaced react-bootstrap with reactstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import characters from './characters.json';
 // import Wrapper from './components/Wrapper';
 import ImgCard from './components/ImgCard';
-import { Grid, Navbar, Jumbotron } from 'react-bootstrap';
+import { Container, Row, Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
 
 class App extends Component {
 	constructor(props) {
@@ -13,33 +13,29 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<Navbar inverse fixedTop>
-					<Grid>
-						<Navbar.Header>
-							<Navbar.Brand>
-								<a href="#brand">React Memory Game</a>
-							</Navbar.Brand>
-						</Navbar.Header>
-						<Navbar.Text pullRight>
-							<span>Score: {this.state.score}</span> | <span>High Score: {this.state.highScore}</span>
-						</Navbar.Text>
-					</Grid>
+				<Navbar fixed="top" color="dark">
+					<NavbarBrand href="/"> 
+						React Memory Game
+					</NavbarBrand>
+					<span>Score: {this.state.score}</span> | <span>High Score: {this.state.highScore}</span>
 				</Navbar>
 				<Jumbotron>
-					<Grid>
+					<Container>
 						<h1>Memory Game</h1>
 						<p>Click on an image to earn points, but don't click on any more than once!</p>
-					</Grid>
+					</Container>
 				</Jumbotron>
-				<Grid>
-					{/* <img src={ require(`${this.state.characters[0].image}`) } alt={this.state.characters[0].name} /> */}
-					{ this.state.characters.map(character => <ImgCard 
-						key={character.id}
-						id={character.id}
-						name={character.name}
-						image={ require(`${character.image}`) }
-					/>) }
-				</Grid>
+				<Container >
+					<Row>
+						{/* <img src={ require(`${this.state.characters[0].image}`) } alt={this.state.characters[0].name} /> */}
+						{ this.state.characters.map(character => <ImgCard 
+							key={character.id}
+							id={character.id}
+							name={character.name}
+							image={ require(`${character.image}`) }
+						/>) }
+					</Row>
+				</Container>
 			</div>
 		);
 	}
--- src/components/ImgCard/ImgCard.js
+++ src/components/ImgCard/ImgCard.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './ImgCard.css';
 
 const ImgCard = props => (
-	<div className='card'>
-
-		<img src={ props.image } alt={ props.name  }/>
-
+	<div className='card col-lg-3 col-md-4 col-xs-6'>
+		<img src={ props.image } alt={ props.name } className='cardImage img-fluid' />
 	</div>
 );
 
