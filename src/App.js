6222f2232a88b5bfeb3a0073f8930af2fad40c2a
Ovieh <ovieh.mosley@gmail.com>
2018-01-13 21:56:20 -0500

    added background to jumbotron and other styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
 import characters from './characters.json';
-// import Wrapper from './components/Wrapper';
 import ImgCard from './components/ImgCard';
 import {Container, Row, Navbar, NavbarBrand, Jumbotron} from 'reactstrap';
 
@@ -22,6 +21,7 @@ class App extends Component {
 				this.setState({characters});
 
 		}
+		//Reactor this monster
 		pickCard = id => {
 				let score = this.state.score;
 				let highScore = this.state.highScore;
@@ -32,7 +32,7 @@ class App extends Component {
 								if (id === character.id) {
 										if (character.clicked) {
 												score = 0;
-												
+												// renderAlert();
 												this.setState({
 														characters: this.resetClicked()
 												});
@@ -57,6 +57,11 @@ class App extends Component {
 				this.setState({characters});
 		}
 
+		//Randomize at Mount
+		componentDidMount() {
+			this.shuffleCards();
+		}
+
 		render() {
 				return (
 						<div>
@@ -78,7 +83,6 @@ class App extends Component {
 								</Jumbotron>
 								<Container >
 										<Row>
-												{/* <img src={ require(`${this.state.characters[0].image}`) } alt={this.state.characters[0].name} /> */}
 												{this
 														.state
 														.characters
